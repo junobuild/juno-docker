e5ec88640bd8b6789c994818fe2288e580e79829
David Dal Busco <david.dalbusco@outlook.com>
2023-12-31 16:32:04 +0100

    feat: try to deploy II

--- cli/src/commands/deploy.ts
+++ cli/src/commands/deploy.ts
@@ -1,3 +1,46 @@
+import {ICManagementCanister, InstallMode} from '@dfinity/ic-management';
+import {Ed25519KeyIdentity} from '@dfinity/identity';
+import {createAgent} from '@dfinity/utils';
+import {createHash} from 'crypto';
+import {readFile} from 'node:fs/promises';
+
 export const deploy = async (args?: string[]) => {
-  console.log('DEPLOY HERE');
+  const identity = Ed25519KeyIdentity.generate();
+  const token = identity.toJSON();
+
+  const loadWasm = async (file: string): Promise<{hash: string; wasm: Buffer}> => {
+    const wasm = await readFile(file);
+
+    return {
+      wasm,
+      hash: createHash('sha256').update(wasm).digest('hex')
+    };
+  };
+
+  const {wasm} = await loadWasm('./target/internet_identity.gz');
+
+  const agent = await createAgent({
+    identity,
+    host: 'http://localhost:5987',
+    fetchRootKey: true
+  });
+
+  const {provisionalCreateCanisterWithCycles, installCode} = ICManagementCanister.create({
+    agent
+  });
+
+  const canisterId = await provisionalCreateCanisterWithCycles({
+    settings: {
+      controllers: [identity.getPrincipal().toString()]
+    }
+  });
+
+  console.log(`🚀 ----> ${canisterId.toString()}`);
+
+  await installCode({
+    mode: InstallMode.Install,
+    canisterId,
+    wasmModule: wasm,
+    arg: new Uint8Array()
+  });
 };
