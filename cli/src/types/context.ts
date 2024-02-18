78c4b840fc4514884bc6633a218fb59e8407df79
David Dal Busco <david.dalbusco@outlook.com>
2024-02-18 16:13:01 +0100

    chore: type

--- cli/src/types/context.ts
+++ cli/src/types/context.ts
@@ -1,9 +1,9 @@
 import type {HttpAgent, Identity} from '@dfinity/agent';
-import type {MAIN_IDENTITY_KEY} from '../constants/constants';
+import {MAIN_IDENTITY_KEY, MINTER_IDENTITY_KEY} from '../constants/constants';
 import type {CliState} from '../states/cli.state';
 
 export interface CliContext {
-  identities: {[MAIN_IDENTITY_KEY]: Identity} & Record<string, Identity>;
+  identities: {[MAIN_IDENTITY_KEY]: Identity, [MINTER_IDENTITY_KEY]: Identity};
   agent: HttpAgent;
   state: CliState;
 }
