9d3f94163f7e4b634c24428b5207dae286f163e2
David Dal Busco <david.dalbusco@outlook.com>
2024-02-18 16:26:10 +0100

    fix: generate different identities

--- cli/src/services/identity.services.ts
+++ cli/src/services/identity.services.ts
@@ -2,7 +2,7 @@ import {Ed25519KeyIdentity} from '@dfinity/identity';
 import {nonNullish} from '@dfinity/utils';
 import type {CliState} from '../states/cli.state';
 
-export const getIdentity = ({state, key}: {state: CliState; key: string}): Ed25519KeyIdentity => {
+export const getIdentity = ({state, key, seed}: {state: CliState; key: string, seed: Uint8Array}): Ed25519KeyIdentity => {
   const identity = state.getIdentity(key);
 
   if (nonNullish(identity)) {
@@ -10,7 +10,7 @@ export const getIdentity = ({state, key}: {state: CliState; key: string}): Ed255
     return Ed25519KeyIdentity.fromParsedJson(token);
   }
 
-  const newIdentity = Ed25519KeyIdentity.generate();
+  const newIdentity = Ed25519KeyIdentity.generate(seed);
   state.saveIdentity({key, jsonIdentity: newIdentity.toJSON()});
 
   return newIdentity;
