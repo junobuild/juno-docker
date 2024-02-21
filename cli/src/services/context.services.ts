b790c248cbf2cc1e3f9fdb431fc5f0d10230ae27
David Dal Busco <david.dalbusco@outlook.com>
2024-02-21 09:10:52 +0100

    feat: workaround with null

--- cli/src/services/context.services.ts
+++ cli/src/services/context.services.ts
@@ -22,23 +22,8 @@ export const buildContext = async (args?: string[]): Promise<CliContext> => {
     throw new Error('An icx-proxy port must be provided as argument of the deploy command.');
   }
 
-  /**
-   * Agent-js seems to require a random (or different) seed to generate different principal when executed in a row.
-   * See https://forum.dfinity.org/t/ed25519keyidentity-generate-without-args-generates-same-principal/27659.
-   */
-  const generateSeed = (index: number): Uint8Array => {
-    if (index >= 256) {
-      throw new Error('This implementation supports only 256 seeds');
-    }
-
-    const bytes: Uint8Array = new Uint8Array(32).fill(0);
-    bytes[31] = index;
-
-    return bytes;
-  };
-
-  const mainIdentity = getIdentity({key: MAIN_IDENTITY_KEY, state, seed: generateSeed(0)});
-  const minterIdentity = getIdentity({key: MINTER_IDENTITY_KEY, state, seed: generateSeed(1)});
+  const mainIdentity = getIdentity({key: MAIN_IDENTITY_KEY, state});
+  const minterIdentity = getIdentity({key: MINTER_IDENTITY_KEY, state});
 
   const agent = await createAgent({
     identity: mainIdentity,
--- cli/src/services/identity.services.ts
+++ cli/src/services/identity.services.ts
@@ -2,15 +2,7 @@ import {Ed25519KeyIdentity} from '@dfinity/identity';
 import {nonNullish} from '@dfinity/utils';
 import type {CliState} from '../states/cli.state';
 
-export const getIdentity = ({
-  state,
-  key,
-  seed
-}: {
-  state: CliState;
-  key: string;
-  seed: Uint8Array;
-}): Ed25519KeyIdentity => {
+export const getIdentity = ({state, key}: {state: CliState; key: string}): Ed25519KeyIdentity => {
   const identity = state.getIdentity(key);
 
   if (nonNullish(identity)) {
@@ -18,7 +10,9 @@ export const getIdentity = ({
     return Ed25519KeyIdentity.fromParsedJson(token);
   }
 
-  const newIdentity = Ed25519KeyIdentity.generate(seed);
+  // TODO: bump agent-js and remove null
+  // Workaround for agent-js issue https://forum.dfinity.org/t/ed25519keyidentity-generate-without-args-generates-same-principal/27659
+  const newIdentity = Ed25519KeyIdentity.generate(null as unknown as Uint8Array | undefined);
   state.saveIdentity({key, jsonIdentity: newIdentity.toJSON()});
 
   return newIdentity;
