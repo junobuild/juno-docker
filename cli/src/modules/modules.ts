f94800dbbb6c4adbda0298060873d945938a00ce
David Dal Busco <david.dalbusco@outlook.com>
2024-02-21 18:44:07 +0100

    feat: add support for index canister

--- cli/src/modules/modules.ts
+++ cli/src/modules/modules.ts
@@ -1,5 +1,6 @@
+import {icpIndex} from './icp-index';
 import {icpLedger} from './icp-ledger';
 import {internetIdentity} from './internet-identity';
 import {satellite} from './satellite';
 
-export const modules = [internetIdentity, icpLedger, satellite];
+export const modules = [internetIdentity, icpLedger, icpIndex, satellite];
