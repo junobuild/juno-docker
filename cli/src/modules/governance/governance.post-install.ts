ecfa9e50511039963654bb47f48beafb894eb106
David Dal Busco <david.dalbusco@outlook.com>
2024-05-12 14:48:38 +0200

    feat: post-install proposal to set xdr icp

--- cli/src/modules/governance/governance.post-install.ts
+++ cli/src/modules/governance/governance.post-install.ts
@@ -1,16 +1,9 @@
-import {
-  GovernanceCanister,
-  NnsFunction,
-  ProposalRewardStatus,
-  ProposalStatus,
-  Topic,
-  type MakeProposalRequest
-} from '@dfinity/nns';
+import {IDL} from '@dfinity/candid';
+import {GovernanceCanister, NnsFunction, type MakeProposalRequest} from '@dfinity/nns';
 import {createAgent} from '@dfinity/utils';
 import {MAIN_IDENTITY_KEY} from '../../constants/constants';
 import type {ModuleInstallParams} from '../../types/module';
-import {NEURON_ID} from "./governance.constants";
-import {enumsExclude} from "./enum.utils";
+import {NEURON_ID} from './governance.constants';
 
 export const makeIcpXdrProposal = async ({identities}: Pick<ModuleInstallParams, 'identities'>) => {
   const {[MAIN_IDENTITY_KEY]: identity} = identities;
@@ -21,24 +14,35 @@ export const makeIcpXdrProposal = async ({identities}: Pick<ModuleInstallParams,
     fetchRootKey: true
   });
 
-  const {makeProposal, listProposals} = GovernanceCanister.create({
+  const {makeProposal} = GovernanceCanister.create({
     agent
   });
 
-  // Example found it some proposal
-  const _payload = {
-    data_source: '{"icp":["Binance"],"sdr":"xe.com"}',
-    timestamp_seconds: 1683500400,
-    xdr_permyriad_per_icp: 41388
-  };
-
-  // Source NNS dapp dummy-proposals.utils.js
-  const exchangeRatePayload = new Uint8Array([
-    68, 73, 68, 76, 1, 108, 3, 144, 203, 139, 170, 1, 113, 223, 245, 129, 160, 8, 120, 214, 213,
-    218, 198, 15, 120, 1, 0, 3, 105, 109, 102, 16, 39, 0, 0, 0, 0, 0, 0, 229, 10, 27, 96, 0, 0, 0, 0
-  ]);
-
-  console.log('------------------------------------------------> BEGIN BEGIN BEGIN BEGIN BEGIN');
+  // Example of payload data found it some proposal
+  const arg = IDL.encode(
+    [
+      IDL.Record({
+        data_source: IDL.Text,
+        timestamp_seconds: IDL.Nat64,
+        xdr_permyriad_per_icp: IDL.Nat64,
+        reason: IDL.Opt(
+          IDL.Variant({
+            OldRate: IDL.Null,
+            DivergedRate: IDL.Null,
+            EnableAutomaticExchangeRateUpdates: IDL.Null
+          })
+        )
+      })
+    ],
+    [
+      {
+        data_source: '{"icp":["Binance"],"sdr":"xe.com"}',
+        timestamp_seconds: BigInt(1683500400),
+        xdr_permyriad_per_icp: BigInt(41388),
+        reason: []
+      }
+    ]
+  );
 
   const request: MakeProposalRequest = {
     neuronId: BigInt(NEURON_ID),
@@ -48,33 +52,10 @@ export const makeIcpXdrProposal = async ({identities}: Pick<ModuleInstallParams,
     action: {
       ExecuteNnsFunction: {
         nnsFunctionId: NnsFunction.IcpXdrConversionRate,
-        payloadBytes: exchangeRatePayload
+        payloadBytes: arg
       }
     }
   };
 
-  const result = await makeProposal(request);
-
-  console.log('------------------------------------------------> END END END END END END END END', result);
-
-  console.log(
-    '---------__________>',
-    await listProposals({
-      request: {
-        limit: 10000,
-        beforeProposal: undefined,
-        excludeTopic: [Topic.Unspecified],
-        includeRewardStatus: enumsExclude({
-          obj: ProposalRewardStatus as unknown as ProposalRewardStatus,
-          values: [ProposalRewardStatus.Unknown]
-        }),
-        includeStatus: enumsExclude({
-          obj: ProposalStatus as unknown as ProposalStatus,
-          values: [ProposalStatus.Unknown]
-        }),
-        includeAllManageNeuronProposals: true
-      },
-      certified: false
-    })
-  );
+  await makeProposal(request);
 };
--- cli/src/modules/governance/index.ts
+++ cli/src/modules/governance/index.ts
@@ -1,7 +1,11 @@
+import {assertNonNullish} from '@dfinity/utils';
+import kleur from 'kleur';
 import {Module} from '../../services/modules.services';
 import type {ModuleDescription, ModuleInstallParams} from '../../types/module';
 import {prepareGovernanceArgs} from './governance.install';
 import {makeIcpXdrProposal} from './governance.post-install';
+
+const {green, cyan} = kleur;
 
 const GOVERNANCE: ModuleDescription = {
   key: 'governance',
@@ -23,6 +27,19 @@ class GovernanceModule extends Module {
 
   override async postInstall(context: ModuleInstallParams): Promise<void> {
     await makeIcpXdrProposal(context);
+
+    const {state} = context;
+
+    const metadata = state.getModule(this.key);
+
+    assertNonNullish(
+      metadata,
+      'Module has not been installed and therefore cannot be post-installed!'
+    );
+
+    const {name, canisterId} = metadata;
+
+    console.log(`🫠  ${green(name)} post-install. ID: ${cyan(canisterId.toString())}`);
   }
 }
 
