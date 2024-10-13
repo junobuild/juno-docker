1cabf443acf2232ea11139ccdcdc03993713096b
David Dal Busco <david.dalbusco@outlook.com>
2024-10-13 13:56:11 +0200

    feat: CMC post-install to authorize to create canisters in subnet

--- cli/src/modules/cmc/index.ts
+++ cli/src/modules/cmc/index.ts
@@ -1,6 +1,11 @@
+import {assertNonNullish} from '@dfinity/utils';
+import kleur from 'kleur';
 import {Module} from '../../services/modules.services';
 import type {ModuleDescription, ModuleInstallParams} from '../../types/module';
 import {prepareCmcArgs} from './cmc.install';
+import {makeAuthorizedSubnetworksProposal} from './cmc.post-install';
+
+const {green, cyan} = kleur;
 
 const CMC: ModuleDescription = {
   key: 'cmc',
@@ -18,6 +23,23 @@ class CmcModule extends Module {
       identities,
       ...rest
     });
+  }
+
+  override async postInstall(context: ModuleInstallParams): Promise<void> {
+    await makeAuthorizedSubnetworksProposal(context);
+
+    const {state} = context;
+
+    const metadata = state.getModule(this.key);
+
+    assertNonNullish(
+      metadata,
+      'Module has not been installed and therefore cannot be post-installed!'
+    );
+
+    const {name, canisterId} = metadata;
+
+    console.log(`🫠  ${green(name)} post-install. ID: ${cyan(canisterId.toString())}`);
   }
 }
 
