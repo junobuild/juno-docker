624664d7ef3d489df6eca51a2aff1d945d1f40ba
David Dal Busco <david.dalbusco@outlook.com>
2024-10-13 15:46:35 +0200

    feat: CMC post-install to authorize to create canisters in subnet (#43)
    * feat: CMC post-install to authorize to create canisters in subnet
    
    * feat: neuronId constant was moved
    
    * fix: nullish
    
    * fix: principal self authenticating

---
+++ cli/src/modules/cmc/cmc.post-install.ts
@@ -0,0 +1,55 @@
+import {IDL} from '@dfinity/candid';
+import {GovernanceCanister, NnsFunction, type MakeProposalRequest} from '@dfinity/nns';
+import {Principal} from '@dfinity/principal';
+import {arrayBufferToUint8Array, createAgent} from '@dfinity/utils';
+import {MAIN_IDENTITY_KEY} from '../../constants/constants';
+import {NEURON_ID} from '../../constants/modules.constants';
+import type {ModuleInstallParams} from '../../types/module';
+
+export const makeAuthorizedSubnetworksProposal = async ({
+  identities
+}: Pick<ModuleInstallParams, 'identities'>) => {
+  const {[MAIN_IDENTITY_KEY]: identity} = identities;
+
+  const agent = await createAgent({
+    identity,
+    host: 'http://127.0.0.1:5987',
+    fetchRootKey: true
+  });
+
+  const {makeProposal} = GovernanceCanister.create({
+    agent
+  });
+
+  const subnetId = Principal.selfAuthenticating(arrayBufferToUint8Array(agent.rootKey));
+
+  const arg = IDL.encode(
+    [
+      IDL.Record({
+        who: IDL.Opt(IDL.Principal),
+        subnets: IDL.Vec(IDL.Principal)
+      })
+    ],
+    [
+      {
+        who: [],
+        subnets: [subnetId]
+      }
+    ]
+  );
+
+  const request: MakeProposalRequest = {
+    neuronId: BigInt(NEURON_ID),
+    url: 'https://forum.dfinity.org',
+    title: 'Authorize CMC to create canisters in subnets',
+    summary: 'The lack of documentation makes developing anything with the CMC canister a pain.',
+    action: {
+      ExecuteNnsFunction: {
+        nnsFunctionId: NnsFunction.SetAuthorizedSubnetworks,
+        payloadBytes: arg
+      }
+    }
+  };
+
+  await makeProposal(request);
+};
