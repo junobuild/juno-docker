cc349d9a89182a16bb282b4f2d1ecfab1a493964
David Dal Busco <david.dalbusco@outlook.com>
2025-05-14 08:04:44 +0200

    chore: fmt

--- cli/src/modules/modules.ts
+++ cli/src/modules/modules.ts
@@ -1,5 +1,5 @@
 import {nonNullish, notEmptyString} from '@dfinity/utils';
-import {Module} from '../services/modules.services';
+import type {Module} from '../services/modules.services';
 import {cmc} from './cmc';
 import {consoleModule} from './console';
 import {governance} from './governance';
