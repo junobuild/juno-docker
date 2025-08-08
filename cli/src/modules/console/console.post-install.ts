6b4ae743de054ad7f6fbf4322787511ebf36f00c
David Dal Busco <david.dalbusco@outlook.com>
2025-08-08 13:32:27 +0200

    feat: post-install latest locally developed version of the modules in the Console

--- cli/src/modules/console/console.post-install.ts
+++ cli/src/modules/console/console.post-install.ts
@@ -1,3 +1,6 @@
+import {existsSync} from 'node:fs';
+import {join} from 'node:path';
+import {DEV_DEPLOY_FOLDER} from '../../constants/dev.constants';
 import {installRelease} from '../../services/modules/console.services';
 import type {ModuleInstallParams} from '../../types/module';
 import {satellite} from '../satellite';
@@ -20,9 +23,16 @@ export const installReleases = async (context: ModuleInstallParams) => {
     },
     {key: ORBITER_KEY, name: ORBITER_NAME}
   ].map(async ({key, name}) => {
+    // Useful for E2E testing. This way the emulator boots with a Console that is loaded
+    // with the latest locally developed version of the modules.
+    const watchWasmPath = join(DEV_DEPLOY_FOLDER, `${key}.wasm.gz`);
+    const stockWasmPath = `./target/${key}.gz`;
+
+    const wasmPath = existsSync(watchWasmPath) ? watchWasmPath : stockWasmPath;
+
     await installRelease({
       context,
-      wasmPath: `./target/${key}.gz`,
+      wasmPath,
       version: '0.0.1',
       key,
       name
