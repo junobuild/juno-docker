29dca70081b83585e1be0ff6834372d2b3b5bb04
David Dal Busco <david.dalbusco@outlook.com>
2026-03-14 11:16:22 +0100

    feat: use latest juno js libs and replace zod by junobuild/schema (#258)

--- cli/src/modules/satellite/satellite.config.ts
+++ cli/src/modules/satellite/satellite.config.ts
@@ -6,7 +6,7 @@ import {
   setSatelliteControllers as setSatelliteControllersAdmin
 } from '@junobuild/admin';
 import type {SatelliteDid, SatelliteParameters} from '@junobuild/ic-client/actor';
-import type {PrincipalText} from '@junobuild/zod';
+import type {PrincipalText} from '@junobuild/schema';
 import {MAIN_IDENTITY_KEY} from '../../constants/constants';
 import type {ControllerScope} from '../../declarations/satellite';
 import type {CliContext} from '../../types/context';
