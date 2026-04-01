ae5e4e12148ad137e424146ba9a8054e83dc02e8
David Dal Busco <david.dalbusco@outlook.com>
2026-04-01 14:16:08 +0200

    feat: remove custom install IC canisters (#264)

--- cli/src/modules/modules.ts
+++ cli/src/modules/modules.ts
@@ -1,26 +1,10 @@
 import {nonNullish, notEmptyString} from '@dfinity/utils';
 import type {Module} from '../services/modules/module.services';
-import {cmc} from './cmc';
 import {consoleModule} from './console';
-import {governance} from './governance';
-import {icpIndex} from './icp-index';
-import {icpLedger} from './icp-ledger';
-import {internetIdentity} from './internet-identity';
 import {observatory} from './observatory';
 import {satellite} from './satellite';
 
-const MODULES = [internetIdentity, icpLedger, icpIndex, satellite, consoleModule, observatory];
-
-// Canisters that require other modules (like the ledger) to be installed first
-// before they can be initialized. Still a mystery how they were deployed at genesis...
-//
-// Also, the order matters. For example:
-// - The CMC must be available when the Governance canister is installed.
-// - The Governance must be available when the CMC post-install runs.
-// - The CMC post-install must runs before the Governance one.
-//
-// Yolo
-const TROUBLEMAKERS = [cmc, governance];
+const MODULES = [satellite, consoleModule, observatory];
 
 const filterModules = (modules: Module[]): Module[] =>
   (process.env.MODULES ?? '')
@@ -30,4 +14,3 @@ const filterModules = (modules: Module[]): Module[] =>
     .filter((mod) => nonNullish(mod));
 
 export const modules = filterModules(MODULES);
-export const troublemakers = filterModules(TROUBLEMAKERS);
